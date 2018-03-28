db1ca3ff5f445df9f4a3231bbe1481bc0d30592d
jrhill95 <32690216+jrhill95@users.noreply.github.com>
2018-03-28 19:40:29 -0400

    Pug Rendering for Similar Artists
    Created pug file and adapted rendering

--- Demo/routes/index.js
+++ Demo/routes/index.js
@@ -5,15 +5,15 @@ const request = require("request");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Concert Recommendation' });
 });
 
 router.get('/artist',function (req, res, next) {
     artist = req.query.artist;
     console.log(artist);
 
-    const u1 = 'https://tastedive.com/api/similar';
-    const u2 = u1 + encodeURIComponent('red+hot+chili+peppers');
+    //const u1 = 'https://tastedive.com/api/similar';
+    //const u2 = u1 + encodeURIComponent('red+hot+chili+peppers');
     //console.log(u2);
 
     const options = {
@@ -38,7 +38,7 @@ router.get('/artist',function (req, res, next) {
           artists.push(artistsJSON[i].Name);
         }
         console.log(artists);
-        res.render('index', { title: 'BTC Rates', result: JSON.parse(body).Similar.Info });
+        res.render('similarartists', { searchartist:artist, similarartists:artists});
 
     })
 
